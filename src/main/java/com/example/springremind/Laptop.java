d7a7bf9cbb6d1385c310864a71365da08d90be73
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2023-09-20 12:28:15 +0530

    init

---
+++ src/main/java/com/example/springremind/Laptop.java
@@ -0,0 +1,12 @@
+package com.example.springremind;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void compile (){
+
+        System.out.println("compiling");
+    }
+}
---
+++ src/main/java/com/example/springremind/SpringRemindApplication.java
@@ -0,0 +1,19 @@
+package com.example.springremind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringRemindApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =  SpringApplication.run(SpringRemindApplication.class, args);
+        Alien obj = context.getBean(Alien.class);
+
+        obj.code();
+        System.out.println("hello");
+    }
+
+}
