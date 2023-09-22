cd14cae5d45546ebfe23a895c779899870f69758
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2023-09-22 12:34:23 +0530

    added rest api demo

---
+++ rest/rest-demo/src/main/java/com/example/restdemo/reminder/ReminderController.java
@@ -0,0 +1,21 @@
+package com.example.restdemo.reminder;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ReminderController {
+    @GetMapping("aliens")
+    @ResponseBody
+    public List<String> getList(){
+        List<String> count = new ArrayList<>();
+        count.add("re");
+        count.add("er");
+        count.add("wwq");
+        return count;
+    }
+}
